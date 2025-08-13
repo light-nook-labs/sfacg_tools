b3bd7b17acad3528cb7a62b4174bcd03a037bfdd
d111kc <d111kc@noreply.gitcode.com>
2025-08-13 10:26:40 +0800

    Aug 13

---
+++ book.py
@@ -0,0 +1,111 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup, Tag, NavigableString
+from loguru import logger
+
+HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+class MobileChapter:
+    """一个章节"""
+    headers = HEADERS
+
+    def __init__(self, title:str, url:str):
+        self.url = url
+        self.title = title
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(title="{self.title}", url="{self.url}")'
+
+    def get_chapter_content(self):
+        """获取章节内容"""
+        logger.info(f'{self.title} {self.url}')
+        response = requests.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        content = f'### {self.title}\n\n'
+        children = [child.name for child in soup.div.div.children]
+        # print(children)
+        for child in soup.div.div.children:
+            # print(type(child.name))
+            if type(child) == NavigableString and str(child).strip() != '':
+                content += f"{str(child).strip()}\n\n"
+            elif child.name == "img":
+                content += f"![]({child['src']})\n\n"
+            elif child.name == "p":
+                content += f"{child.get_text()}\n\n"
+            elif child.name == "br":
+                continue
+        self.content = content.strip()
+        return content.strip()
+
+class Volumn:
+    """卷"""
+    headers = HEADERS
+    base_url = 'https://m.sfacg.com'
+    def __init__(self, title: str, vol_tag: Tag):
+        self.title = title
+        self.vol_tag = vol_tag.next_sibling.next_sibling.ul
+
+    def get_volumn_content(self):
+        """获取本卷内容"""
+        logger.info(f'{self.title}')
+        volumn_content = f'## {self.title}\n\n'
+        chapters = {}
+        for a_tag in self.vol_tag.find_all('a'):
+            # print(a_tag)
+            chapters[a_tag.get_text()] = self.base_url + a_tag['href']
+        # print(chapters)
+        for chapter_title, chapter_url in chapters.items():
+            chapter = MobileChapter(chapter_title, chapter_url)
+            chapter_content = chapter.get_chapter_content()
+            volumn_content += chapter_content + '\n\n'
+        return volumn_content
+
+class Novel:
+    """小说内容"""
+    headers = HEADERS
+    base_url_index = 'https://m.sfacg.com/b/'
+    base_url_menu = 'https://m.sfacg.com/i/'
+
+    def __init__(self, nid: int):
+        self.nid = nid
+        self.index_url = ''
+        self.title = ''
+        self.label = ''
+        self.author = ''
+        self.word_num = 0
+        self.click_num = 0
+        self.date = ''
+        self.clock = ''
+        self.heart_num = 0
+        self.praise_num = 0
+        self.intro = ''
+        self._get_novel_info()
+        self.latest = f'最后更新时间：{self.date} {self.clock}'
+
+    def _get_novel_info(self):
+        """获取小说信息"""
+        self.index_url = self.base_url_index + str(self.nid)
+        res = requests.get(url=self.index_url, headers=self.headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        info_tag = soup.find(class_='book_info')
+        self.title = info_tag.span.string
+        for part in info_tag.div.stripped_strings:
+            self.label += part + ' '
+        self.author, self.word_num, click_and_new = soup.find(class_='book_info3').get_text().split(' / ')
+        self.click_num, self.date, self.clock = click_and_new.split()
+        smalls = soup.find_all('small')
+        self.heart_num, self.praise_num, _ = [small.string.strip() for small in smalls]
+        self.intro = soup.find(class_='book_bk_qs1').string
+
+if __name__ == '__main__':
+    # url = 'https://m.sfacg.com/c/8393500/'
+    url = 'https://m.sfacg.com/c/9090775/'
+    # url = 'https://book.sfacg.com/Novel/744362/985558/9090775/'
+    novel_url = 'https://m.sfacg.com/b/751089/'
+    nid = 751089
+    novel = Novel(nid)
+    print(novel.latest)
+
+
